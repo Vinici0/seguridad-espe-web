19069a9beba50a0b3b1682ffa675208ec80ba9cb
Kevin <knpaute@espe.edu.ec>
2023-07-04 08:42:32 -0500

    majo2

--- pages/filosofia/index.tsx
+++ pages/filosofia/index.tsx
@@ -6,44 +6,48 @@ const EquipoPage = () => {
   return (
     <>
       <Layout title="Filosofía">
-      <div className="blob1"></div>
-        <div className="blob2"></div>
-        
         <section className="relative">
-          <div className="container py-20 bg-slate-400 ">
-            <div className="flex flex-row items-center justify-center bg-slate-300 w-2/4 max-sm:w-full max-sm:flex-col max-sm:items-start">
-              <div className="flex flex-row items-center justify-center space-x-4  w-2/4 px-5 mx-7 ">
-                <Image
-                  src="https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg"
-                  width={300}
-                  height={300}
-                  alt="ad"
-                  className="max-sm:rounded-full w-20 h-20 rounded-none max-sm:w-10 max-sm:h-10"
-                />
-                <p>Vincio Borja</p>
-              </div>
-              <div className="flex flex-row items-center justify-center space-x-4 m-10"> 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aliquid a voluptate repellat? Sed repudiandae voluptatum, impedit quae sint facilis unde cupiditate similique? Sequi porro debitis impedit nam odit dolorum.</p>
+          <div className="blob1"></div>
+          <div className="blob2"></div>
+          <div className="container py-20">
+
+            <div className="flex flex-col items-center z-20 ">
+              <div className="text-center md:text-center md:w-1/2 md:pr-10">
+                <h1 className="title text-3xl color-gray-light font-bold">
+                  Universidad de las Fuerzas Armadas ESPE
+                </h1>
               </div>
             </div>
           </div>
         </section>
-        <section id="saving-money" className="bg-color-secondary">
-        <div className="container py-8 relative">
-          <div className="flex flex-col items-center justify-center mb-4">
-            <p className="leading-relaxed text-xl font-bold text-center text-color-primary">
-              INFORME DE INCIDENTES DE LA PARROQUIA LUZ DE AMÉRICA
-            </p>
-            <div className="flex mt-4">
-              <button className="bg-red-500 text-white px-4 py-2 mr-4">
-                Descargar PDF
-              </button>
-              <button className="bg-green-500 text-white px-4 py-2">
-                Descargar CSV
-              </button>
+        <section id="saving-money" className="bg-color-secondary" style={{ backgroundColor: "#E0D9F1" }}>
+          <div className="container py-20 relative">
+            <h1 className="title mb-4 text-3xl color-gray-light font-bold  text-center ">
+              Filosofía Institucional
+            </h1>
+            <div className="flex flex-col items-center justify-center md:flex-row  md:ml-20 mr-20">
+              {/* <div className="mb-1 md:w-2/4 ">
+              <Image
+                src="/images/menusession2.webp"
+                width={500}
+                height={500}
+                alt="hero"
+                className="rounded-lg"
+              />
+            </div> */}
+              <div className="text-center md:text-left md:w-50 md:ml-12">
+                <p className=" ">
+                  La institución se debe fundamentalmente a la sociedad y Fuerzas Armadas, a ellas orienta su esfuerzo, contribuyendo a la solución de sus necesidades de desarrollo, mediante la formación profesional, técnica y científica, la investigación, planteamiento e implementación de soluciones en el ámbito de sus dominios académicos. Con la búsqueda permanente de la excelencia como institución centenaria y referente en la sociedad, mediante la práctica de los principios: respeto por la dignidad humana, identidad ESPE, excelencia académica; y los valores: disciplina, integridad, liderazgo, creatividad, cohesión y efectividad, en todas sus actividades académicas y administrativas, aplicando métodos y herramientas efectivas para la integración de la docencia, investigación y vinculación con la sociedad.
+
+                </p>
+                <br />
+                <p className="">
+                La formación de profesionales se fundamenta en proyectos multidisciplinarios relacionados al paradigma “Smart University” que incluye el desarrollo de una universidad postmoderna con énfasis en la aplicación del gobierno electrónico (e-gobierno), internacionalización y modelo educativo innovador, tendiente a continuar como institución de educación superior de las Fuerzas Armadas, referente en el ámbito nacional e internacional.
+
+                </p>
+              </div>
             </div>
           </div>
-        </div>
         </section>
       </Layout>
     </>
